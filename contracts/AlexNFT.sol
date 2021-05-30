bf51afd6f7a3cfdfa033bf553123dc043c3c88e3
Alex Georgousis <alexgeo2395@gmail.com>
2021-05-30 11:08:57 +0100

    First version of nft (not implementing ERC yet)

--- contracts/AlexNFT.sol
+++ contracts/AlexNFT.sol
@@ -3,5 +3,17 @@ pragma solidity ^0.8.0;
 
 
 contract AlexNFT {
-
+    string public nftName;
+    address public owner;
+    
+    constructor(string memory _nftName) {
+        nftName = _nftName;
+    }
+    
+    event nftClaimed(address owner);
+    
+    function claimNFT() public {
+        owner = msg.sender;
+        emit nftClaimed(owner);
+    }
 }
