e6dd24da8cb7b6cbf5f03c3510a7eb78be5ad67d
Alex Georgousis <alexgeo2395@gmail.com>
2021-05-31 14:30:05 +0100

    Set up to deploy to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "midnight sausage spice maple return indoor gravity palace affair eight maze steak";
+
 module.exports = {
   contracts_build_directory: "./client/src/contracts",
   networks: {
@@ -5,6 +8,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/aaf25bf2a6ac4515b0642df0761ac92a")
+      },
+      network_id: 4
     }
   },
   compilers: {
